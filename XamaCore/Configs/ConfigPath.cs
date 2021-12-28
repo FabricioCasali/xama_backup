3ef3f3514d6eadd9c7b0869eb247157548298709
Fabricio Casali <fabriciocasali@gmail.com>
2021-12-28 17:02:17 -0300

    Separação do projeto

--- XamaCore/Configs/ConfigPath.cs
+++ XamaCore/Configs/ConfigPath.cs
@@ -4,8 +4,16 @@ namespace XamaCore.Configs
 {
     public class ConfigPath
     {
+        public ConfigPath()
+        {
+            IncludeSubfolders = true;
+        }
+
         [JsonProperty("path")]
         public string Path { get; set; }
+
+        [JsonProperty("includesSubfolders")]
+        public bool IncludeSubfolders { get; set; }
 
     }
 }
